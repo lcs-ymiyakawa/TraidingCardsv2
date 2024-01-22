591b9fdfe3228d9566b5d936e0b993834b961d36
Yoshi Miyakawa <ymiyakawa@lcs.on.ca>
2024-01-22 08:56:18 -0500

    Initial Commit

---
+++ TraidingCardsv2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TraidingCardsv2
+//
+//  Created by 宮川義之助 on 2024/01/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TraidingCardsv2/TraidingCardsv2App.swift
@@ -0,0 +1,17 @@
+//
+//  TraidingCardsv2App.swift
+//  TraidingCardsv2
+//
+//  Created by 宮川義之助 on 2024/01/22.
+//
+
+import SwiftUI
+
+@main
+struct TraidingCardsv2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
